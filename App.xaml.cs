cd87c316d1111cd774d707d1c4b3f5b25c19770b
Tanakiin <tanakinswagwalker@gmail.com>
2026-01-05 03:48:39 -0500

    Fixed file structure and icon management

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,14 @@ namespace VisorView
 {
     public partial class App : System.Windows.Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += (_, e) =>
+            {
+                System.Windows.MessageBox.Show(e.Exception.ToString(), "Startup crash");
+                e.Handled = true;
+            };
+        }
 
     }
 }
